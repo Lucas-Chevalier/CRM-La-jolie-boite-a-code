facbf68607f7cc4279bf4be28b2aed1d6d5c800c
Lucas-Chevalier <chevalier.lucas.mail@gmail.com>
2023-03-10 16:48:33 +0100

    Finalisation du système de connexion + ajout PDF

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,7 @@
+import random
 import time
 from fpdf import FPDF
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, send_file
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
 from wtforms import StringField, SubmitField, BooleanField, IntegerField, \
@@ -8,6 +9,11 @@ from wtforms import StringField, SubmitField, BooleanField, IntegerField, \
 from wtforms.validators import DataRequired, NumberRange
 import sqlite3
 from form import *
+import os
+from datetime import datetime
+from weasyprint import HTML
+import io
+import uuid
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'FULL FLEMME'
@@ -19,29 +25,36 @@ database_filename = 'CRM.sqlite'
 @app.route('/CRM_Boite_a_code/login', methods=['GET', 'POST'])
 def login():
     error = None
+    database_conn = sqlite3.connect(database_filename, check_same_thread=False)
+    cur = database_conn.cursor()
+    query = "SELECT Login, Password FROM User"
+    list = cur.execute(query).fetchall()
+    result = []
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('index'))
+        for result in list:
+            if request.form['username'] != result[0] or request.form['password'] != result[1]:
+                error = 'Invalid Credentials. Please try again.'
+            else:
+                cur.close()
+                return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
 
 @app.route('/CRM_Boite_a_code/add_login', methods=['GET', 'POST'])
-def add_login():
+def addlogin():
     error = None
     if request.method == 'POST':
         database_conn = sqlite3.connect(database_filename, check_same_thread=False)
         cur = database_conn.cursor()
         query = "INSERT INTO User (Name, Login, Password) VALUES " \
-        f"('{request.form['username']}','{request.form['login']}','{request.form['password']}')"
+        f"('{request.form['username']}','{request.form['login']}','{request.form['password']}');"
         print(query)
         cur.execute(query)
-        time.sleep(1)
+        database_conn.commit()
+        #time.sleep(1)
         cur.close()
-        error = query
-        #database_conn.close()
-        #return redirect(location="/CRM_Boite_a_code/add_prospect")
+        database_conn.close()
+        return redirect(location="/CRM_Boite_a_code/add_prospect")
     return render_template('add_login.html', error=error)
 
 
@@ -57,10 +70,10 @@ def addprospect():
                 f" '{form.City.data}','{form.Description.data}', '{form.Url.data}')"
         #print("aaaaaaa")
         cur = database_conn.cursor()
-        test = cur.execute(query)
-        #database_conn.execute(query)
+        cur.execute(query)
+        database_conn.commit()
         cur.close()
-        database_conn.close();
+        database_conn.close()
         return redirect(location="/CRM_Boite_a_code/add_comment")
 
     return render_template('titanic.html', form=form)
@@ -77,8 +90,9 @@ def addcontact():
                 f" '{form.Phone.data}','{form.Status.data}')"
         cur = database_conn.cursor()
         cur.execute(query)
+        database_conn.commit()
         cur.close()
-        database_conn.close();
+        database_conn.close()
         return redirect(location="/CRM_Boite_a_code/add_comment")
     return render_template('titanic.html', form=form)
 
@@ -94,8 +108,9 @@ def modifycontact():
                 f" '{form.Phone.data}','{form.Status.data}')"
         cur = database_conn.cursor()
         cur.execute(query)
+        database_conn.commit()
         cur.close()
-        database_conn.close();
+        database_conn.close()
         return redirect(location="/CRM_Boite_a_code/add_comment")
     return render_template('titanic.html', form=form)
 
@@ -110,8 +125,9 @@ def addcomment():
                 f"('{form.Description.data}')"
         cur = database_conn.cursor()
         cur.execute(query)
+        database_conn.commit()
         cur.close()
-        database_conn.close();
+        database_conn.close()
         return redirect(location="/CRM_Boite_a_code/add_prospect")
     return render_template('titanic.html', form=form)
 
@@ -123,6 +139,48 @@ def index():
     list = cur.execute(query)
     cur.close()
     return render_template('titanic_list.html', list=list)
+
+@app.route('/CRM_Boite_a_code/invoice')
+def invoive():
+    today = datetime.today().strftime("%d/%m/%Y")
+    invoice_number = 123
+    from_addr = {
+        'company_name': 'Python Tip',
+        'addr1': '12345 Sunny Road',
+        'addr2': 'Sunnyville, CA 12345'
+    }
+    to_addr = {
+        'company_name': 'Acme Corp',
+        'person_name': 'John Dilly',
+        'person_email': 'john@example.com'
+    }
+    items = [
+        {
+            'title': 'website design',
+            'charge': 300.00
+        }, {
+            'title': 'Hosting (3 months)',
+            'charge': 75.00
+        }, {
+            'title': 'Domain name (1 year)',
+            'charge': 10.00
+        }
+    ]
+    duedate = datetime.today().strftime("%d/%m/%Y")
+    total = sum([i['charge'] for i in items])
+    rendered = render_template('invoice.html',
+                               date=today,
+                               from_addr=from_addr,
+                               to_addr=to_addr,
+                               items=items,
+                               total=total,
+                               invoice_number=invoice_number,
+                               duedate=duedate)
+    html = HTML(string=rendered)
+    rendered_pdf = html.write_pdf('./PDF/invoice.pdf')
+    return send_file(
+        './PDF/invoice.pdf'
+    )
 
 
 # Press the green button in the gutter to run the script.
